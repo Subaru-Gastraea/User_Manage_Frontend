d3c6b8e4bcab7284dc3a70c25c248ba51917c265
Subaru <davidqxzz123@gmail.com>
2023-02-03 17:46:14 +0800

    feat(App.jsx): Try to get message from FastAPI testing endpoint "/api" and show on webpage
    Follow the video tutorial: https://www.youtube.com/watch?v=NeKm4zOVkO4&list=PLhH3UpV2flrwfJ2aSwn8MkCKz9VzO-1P4&index=4

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import React, {useState} from "react";
+import {useEffect} from "react";
+
 const App = () => {
+  const [message, setMessage] = useState("");
+
+  const getWelMsg = async () => {
+    const requestOp = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await fetch("/api", requestOp);
+    const data = await response.json();
+
+    setMessage(data.message);
+  };
+
+  useEffect(() => {
+    getWelMsg();
+  }, [])
+
   return (
     <div>
       <h1>User Manage Website</h1>
+      <p>{message}</p>
     </div>
   );
 }
