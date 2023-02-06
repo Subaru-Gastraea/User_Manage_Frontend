6465098438b8cf906521049959d610aec47ff2e3
Subaru <davidqxzz123@gmail.com>
2023-02-06 10:26:42 +0800

    feat(Login.jsx): Add a form with username and password fields
    Login with input username and password, set the token if login successfully

---
+++ src/components/Login.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useContext } from "react";
+
+import ErrorMessage from "./ErrorMessage";
+import { UserContext } from "../context/UserContext";
+
+const Login = () => {
+    const [uname, setUname] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const [, setToken] = useContext(UserContext);
+
+    const submitLogin = async () => {
+        const requestOp = {
+            method: "POST",
+            headers: {"Content-Type": "application/x-www-form-urlencoded"},
+            body: JSON.stringify(
+                `grant_type=&username=${uname}&password=${password}&scope=&client_id=&client_secret=`
+            ),
+        };
+
+        const response = await fetch("/login", requestOp);
+        const data = await response.json();
+
+        if (!response.ok) {
+            setErrorMessage(data.detail);
+        } else {
+            setToken(data.access_token);
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        submitLogin();
+    };
+
+    return (
+        <div className="column">
+            <form className="box" onSubmit={handleSubmit}>
+                <h1 className="title has-text-centered">Login</h1>
+                <div className="field">
+                    <label className="label">User name</label>
+                    <div className="control">
+                        <input
+                            type="uname"
+                            placeholder="Enter user name"
+                            value={uname}
+                            onChange={(e) => setUname(e.target.value)}
+                            className="input"
+                            required
+                        />
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label">Password</label>
+                    <div className="control">
+                        <input
+                            type="password"
+                            placeholder="Enter password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="input"
+                            required
+                        />
+                    </div>
+                </div>
+                <ErrorMessage message={errorMessage} />
+                <br />
+                <button className="button is-primary" type="submit">
+                    Login
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
