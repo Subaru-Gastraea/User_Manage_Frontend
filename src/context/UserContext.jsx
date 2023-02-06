4275ff922f7cc8d63313969290e9d6b58dc96da6
Subaru <davidqxzz123@gmail.com>
2023-02-06 09:05:33 +0800

    feat(UserContext.jsx): Handling token authorization
    Store the token in the local storage for authorization

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,32 @@
+import React, { createContext, useEffect, useState } from "react";
+
+export const UserContext = createContext();
+
+export const UserProvider = (props) => {
+    const [token, setToken] = useState(localStorage.getItem("userToken"));
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const requestOp = {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: "Bearer " + token,
+                },
+            };
+
+            const response = await fetch("/user/", requestOp);
+            if (!response.ok) {
+                setToken(null);
+            }
+            localStorage.setItem("userToken", token);
+        };
+        fetchUser();
+    }, [token]);
+
+    return (
+        <UserContext.Provider value={[token, setToken]}>
+            {props.children}
+        </UserContext.Provider>
+    )
+};
