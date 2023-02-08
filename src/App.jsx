c826df3586a3a4dd6ac6d4b73d33732ed633e5f8
Subaru <davidqxzz123@gmail.com>
2023-02-08 22:35:34 +0800

    fix(App.jsx): Delete testing GET request
    The GET request was used to test interaction between React frontend and FastAPI backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 
 import Login from "./components/Login";
 import Header from "./components/Header";
@@ -6,29 +6,11 @@ import Table from "./components/Table";
 import { UserContext } from "./context/UserContext";
 
 const App = () => {
-  const [message, setMessage] = useState("");
   const [token] = useContext(UserContext);
-
-  const getWelMsg = async () => {
-    const requestOp = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-    const response = await fetch("/api", requestOp);
-    const data = await response.json();
-
-    setMessage(data.message);
-  };
-
-  useEffect(() => {
-    getWelMsg();
-  }, [])
 
   return (
     <>
-      <Header title={message} />
+      <Header title="User Manage Website" />
       <div className="columns is-centered">
         <div className="column m-5 is-two-thirds">
           {!token ? (
